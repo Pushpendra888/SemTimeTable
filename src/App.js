90c914e9e7d1a37ccf7bfe7a42ecbf6975c2d802
unknown <pushpendraanu0@gmail.com>
2024-08-11 20:42:20 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './Components/Footer/Footer';
+// import About from './Components/About/About';
+import Header from './Components/Header/Header';
+import Selector from './Components/Selector/Selector';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header/>
+      <Selector/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
